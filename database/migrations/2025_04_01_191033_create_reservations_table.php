bbbb3fdde2d7834eb3079b0979d9e704c3be82b2
Prog <prog64650@gmail.com>
2025-04-02 18:58:27 +0500

    1.0.7 commit

--- database/migrations/2025_04_01_191033_create_reservations_table.php
+++ database/migrations/2025_04_01_191033_create_reservations_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->index()->constrained()->cascadeOnDelete();
             $table->foreignId('resturant_id')->index()->constrained()->cascadeOnDelete();
             $table->date('date');
+            $table->time('time');
         });
     }
 

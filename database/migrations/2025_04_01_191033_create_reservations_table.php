67a74ace33d67ac579642f05fbcbc38f4b17db8c
Prog <prog64650@gmail.com>
2025-04-02 18:51:35 +0500

    1.0.5 commit

--- database/migrations/2025_04_01_191033_create_reservations_table.php
+++ database/migrations/2025_04_01_191033_create_reservations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->index()->constrained()->cascadeOnDelete();
+            $table->foreignId('resturant_id')->index()->constrained()->cascadeOnDelete();
 
         });
     }
